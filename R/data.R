01248d0948e125f1dc51e68817be75dcf5dfbb70
PollyHung <105984864+PollyHung@users.noreply.github.com>
2025-04-07 16:07:45 +0800

    Update

---
+++ R/data.R
@@ -0,0 +1,196 @@
+#' @title bladder
+#'
+#' @description Breakpoint File for TCGA Bladder Cancer
+#'
+#' @format ## bladder
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/bladder.txt>
+"bladder"
+
+
+#' @title breast
+#'
+#' @description Breakpoint File for TCGA Breast Cancer
+#'
+#' @format ## breast
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/breast.txt>
+"breast"
+
+
+#' @title colorectal
+#'
+#' @description Breakpoint File for TCGA Colorectal Cancer
+#'
+#' @format ## colorectal
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/colorectal.txt>
+"colorectal"
+
+
+#' @title esophageal
+#'
+#' @description Breakpoint File for TCGA Esophageal Cancer
+#'
+#' @format ## esophageal
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/esophageal.txt>
+"esophageal"
+
+
+#' @title glioblastoma
+#'
+#' @description Breakpoint File for TCGA Glioblastoma Cancer
+#'
+#' @format ## glioblastoma
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/glioblastoma.txt>
+"glioblastoma"
+
+
+#' @title head_and_neck
+#'
+#' @description Breakpoint File for TCGA Head and Neck Cancer
+#'
+#' @format ## head_and_neck
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/head_and_neck.txt>
+"head_and_neck"
+
+
+#' @title kidney_clear_cell
+#'
+#' @description Breakpoint File for TCGA Kidney Clear Cell Carcinoma
+#'
+#' @format ## kidney_clear_cell
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/kidney_clear_cell.txt>
+"kidney_clear_cell"
+
+
+#' @title low_grade_glioma
+#'
+#' @description Breakpoint File for TCGA Low Grade Glioma and Glioblastoma
+#'
+#' @format ## low_grade_glioma
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/low_grade_glioma.txt>
+"low_grade_glioma"
+
+
+#' @title ovarian
+#'
+#' @description Breakpoint File for TCGA Ovarian Cancer
+#'
+#' @format ## ovarian
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/ovarian.txt>
+"ovarian"
+
+
+#' @title pancancer
+#'
+#' @description Breakpoint File for TCGA PanCancer
+#'
+#' @format ## pancancer
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{arm}{chromosome arm}
+#'   \item{cytoband}{cytobands}
+#'   \item{start}{start base of breakpoint region}
+#'   \item{end}{end base of breakpoint region}
+#'   \item{direction}{direction of the breakpoint conferring advantage}
+#'   \item{telcent}{direction of the breakpoint calculated, telomere to centromere or opposite}
+#'   \item{id}{combined id of each breakpoint event}
+#' }
+#' @source <https://www.github.com/PollyHung/BAGEL/docs/pancancer.txt>
+"pancancer"
+
+
+
+
+
+
+
+
--- R/hello.R
+++ R/hello.R
@@ -16,3 +16,9 @@
 hello <- function() {
   print("Hello, world! Hello!!")
 }
+
+
+
+
+
+
